dc1c5dd729d80b4b8ee82c3d265a4a45e6c0c89b
Alex <alexjrussian@gmail.com>
2020-01-21 13:22:27 -0500

    react, webpack, babel boilerplate created

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
