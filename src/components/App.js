72bd2bc7337edb538da312b8040fc6f505e99fc4
Alex <alexjrussian@gmail.com>
2020-01-22 17:41:37 -0500

    refactored axios call

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import DayCard from '../containers/DayCard/DayCard';
 import bg from '../assets/img/river-mountain.jpg';
@@ -15,15 +15,73 @@ const Container = styled.div`
 
 const Title = styled.h1`
   font-size: 3rem;
-  margin: 1rem 0;
+  margin: 2rem 0;
   color: #fff;
 `;
 
+const Subtitle = styled.h2`
+  font-size: 2rem;
+  color: #fff;
+  font-weight: 400;
+  font-style: italic;
+  margin-bottom: 0.2rem;
+`;
+
+const Input = styled.input`
+  padding: 0.5rem 2rem;
+  font-size: 2rem;
+  border: 1px solid #eee;
+  background-color: rgba(255, 255, 255, 0.7);
+`;
+
+const Button = styled.button`
+  padding: 1rem 1.5rem;
+  background-color: #fff;
+  border: none;
+  cursor: pointer;
+  color: black;
+  border-radius: 0.3rem;
+  margin: 0.5rem 0;
+`;
+
 const App = () => {
+  const [zip, setZip] = useState('');
+  const [showDayCard, setShowDayCard] = useState(false);
+  const [submitZip, setSubmitZip] = useState('');
+
+  const onInputChangedHandler = e => {
+    setZip(e.target.value);
+  };
+
+  const onClickZipSubmit = () => {
+    checkZip(zip);
+  };
+
+  const checkZip = z => {
+    if (z.length === 5) {
+      setSubmitZip(zip);
+      setShowDayCard(true);
+    }
+  };
+
+  let card = null;
+  if (showDayCard) {
+    card = <DayCard zip={submitZip} />;
+  }
+
   return (
     <Container>
       <Title>What's the Weather like?</Title>
-      <DayCard />
+      <Subtitle>(enter US zip code only, please)</Subtitle>
+      <Input
+        maxLength="5"
+        placeholder="Show me the weather in..."
+        onChange={e => onInputChangedHandler(e)}
+      />
+      <Button type="button" onClick={onClickZipSubmit}>
+        Let's see it!
+      </Button>
+      {card}
     </Container>
   );
 };
--- src/containers/DayCard/DayCard.js
+++ src/containers/DayCard/DayCard.js
@@ -28,15 +28,26 @@ const Time = styled.h4`
 const APP_ID = '69911451';
 const APP_KEY = '4a2ac00be479232fe1d392bb09dae7f3';
 
-const DayCard = () => {
-  const [coords, setCoords] = useState({ lat: '42.361', long: '-71.057' });
-  const [currentTemp, setCurrentTemp] = useState('55');
-  const [feelsLike, setFeelsLike] = useState('56');
-  const [highTemp, setHighTemp] = useState('65');
-  const [lowTemp, setLowTemp] = useState('34');
-  const [sunrise, setSunrise] = useState('7:05 AM');
-  const [sunset, setSunset] = useState('5:55 PM');
+const DayCard = props => {
+  // const [coords, setCoords] = useState({ lat: '42.361', long: '-71.057' });
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentTemp, setCurrentTemp] = useState('...');
+  const [feelsLike, setFeelsLike] = useState('...');
+  const [highTemp, setHighTemp] = useState('...');
+  const [lowTemp, setLowTemp] = useState('...');
+  const [sunrise, setSunrise] = useState('...');
+  const [sunset, setSunset] = useState('...');
   const [wxCode, setWxCode] = useState(0);
+
+  const resetData = () => {
+    setCurrentTemp('...');
+    setFeelsLike('...');
+    setHighTemp('...');
+    setLowTemp('...');
+    setSunrise('...');
+    setSunset('...');
+    setWxCode('...');
+  };
 
   const timeNow = new Date();
   const hours = timeNow.getHours();
@@ -56,57 +67,61 @@ const DayCard = () => {
     return `${hours}:${newMin} AM`;
   };
 
+  const forecast = `http://api.weatherunlocked.com/api/forecast/us.${props.zip}?app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const current = `http://api.weatherunlocked.com/api/current/us.${props.zip}?app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   const getData = () => {
-    console.log('[getData]');
-    // axios
-    //   .get(
-    //     `http://api.weatherunlocked.com/api/current/${coords.lat},${coords.long}?app_id=${APP_ID}&app_key=${APP_KEY}`
-    //   )
-    //   .then(response => {
-    //     setCurrentTemp(`${response.data.temp_f.toFixed(0)}°`);
-    //     setFeelsLike(`${response.data.feelslike_f.toFixed(0)}°`);
-    //     setWxCode(response.data.wx_code);
-    //   });
-    // axios
-    //   .get(
-    //     `http://api.weatherunlocked.com/api/forecast/${coords.lat},${coords.long}?app_id=${APP_ID}&app_key=${APP_KEY}`
-    //   )
-    //   .then(response => {
-    //     const sunriseData = response.data.Days[1].sunrise_time;
-    //     const newSunrise = sunriseData.slice(1);
-    //     setSunrise(`${newSunrise}AM`);
-    //     const sunsetData = response.data.Days[1].sunset_time;
-    //     const sunsetFirst = sunsetData.slice(0, 2);
-    //     const sunsetSecond = sunsetData.slice(2);
-    //     const newSunset = sunsetFirst - 12 + sunsetSecond;
-    //     setSunset(`${newSunset}PM`);
-    //     setHighTemp(`${response.data.Days[1].temp_max_f.toFixed(0)}°`);
-    //     setLowTemp(`${response.data.Days[1].temp_min_f.toFixed(0)}°`);
-    //   });
+    const getCurrent = () => {
+      return axios.get(current);
+    };
+    const getForecast = () => {
+      return axios.get(forecast);
+    };
+
+    axios.all([getCurrent(), getForecast()]).then(
+      axios.spread((cur, fore) => {
+        setCurrentTemp(`${cur.data.temp_f.toFixed(0)}°`);
+        setFeelsLike(`${cur.data.feelslike_f.toFixed(0)}°`);
+        setWxCode(cur.data.wx_code);
+        const sunriseData = fore.data.Days[1].sunrise_time;
+        const newSunrise = sunriseData.slice(1);
+        setSunrise(`${newSunrise}AM`);
+        const sunsetData = fore.data.Days[1].sunset_time;
+        const sunsetFirst = sunsetData.slice(0, 2);
+        const sunsetSecond = sunsetData.slice(2);
+        const newSunset = sunsetFirst - 12 + sunsetSecond;
+        setSunset(`${newSunset}PM`);
+        setHighTemp(`${fore.data.Days[1].temp_max_f.toFixed(0)}°`);
+        setLowTemp(`${fore.data.Days[1].temp_min_f.toFixed(0)}°`);
+        setIsLoading(false);
+      })
+    );
   };
 
-  const showPosition = position => {
-    const newLat = position.coords.latitude.toFixed(2);
-    const newLong = position.coords.longitude.toFixed(2);
-    setCoords({ lat: newLat, long: newLong });
-  };
+  // const showPosition = position => {
+  //   const newLat = position.coords.latitude.toFixed(2);
+  //   const newLong = position.coords.longitude.toFixed(2);
+  //   setCoords({ lat: newLat, long: newLong });
+  // };
 
-  const noPosition = () => {
-    // eslint-disable-next-line no-alert
-    alert('Please enable location services in order to receive weather data for your location');
-  };
+  // const noPosition = () => {
+  //   // eslint-disable-next-line no-alert
+  //   alert('Please enable location services in order to receive weather data for your location');
+  // };
 
-  useEffect(() => {
-    // eslint-disable-next-line no-undef
-    if (navigator.geolocation) {
-      // eslint-disable-next-line no-undef
-      navigator.geolocation.getCurrentPosition(showPosition, noPosition);
-    }
-  }, []);
+  // useEffect(() => {
+  //   // eslint-disable-next-line no-undef
+  //   if (navigator.geolocation) {
+  //     // eslint-disable-next-line no-undef
+  //     navigator.geolocation.getCurrentPosition(showPosition, noPosition);
+  //   }
+  // }, []);
 
   useEffect(() => {
     getData();
-  }, [coords]);
+
+    return resetData();
+  }, [props.zip]);
 
   return (
     <Card>
