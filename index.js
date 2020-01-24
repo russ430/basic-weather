1f82b40b2870cc7827cfb40c612fd20da40a541c
Alex <alexjrussian@gmail.com>
2020-01-24 16:45:58 -0500

    added path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fetch = require('node-fetch');
-
+const path = require('path');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -32,6 +32,10 @@ app.get('/weather/cur/:z', async (req, res) => {
 });
 
 app.use(express.static('client/build'));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Running on ${port}`));
 
