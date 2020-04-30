45c973dbe280951d13bba25ea579f8c103699506
Alex <alexjrussian@gmail.com>
2020-04-30 17:20:31 -0400

    removed build folder from gitignore

---
+++ client/build/precache-manifest.10b408810360727e8f0d4b1af6e665f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e190cf5c7df2aa324c9113360b603fff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a456613627e670b62c7",
+    "url": "/static/css/main.0b382822.chunk.css"
+  },
+  {
+    "revision": "15d10f78ef50180235e4",
+    "url": "/static/js/2.f85d46c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f85d46c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a456613627e670b62c7",
+    "url": "/static/js/main.ec32038f.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  },
+  {
+    "revision": "192a6620a687a9e7e51c0037973a1547",
+    "url": "/static/media/github.192a6620.svg"
+  },
+  {
+    "revision": "3582b69f733b14823ced29599a37710f",
+    "url": "/static/media/partly-cl.3582b69f.svg"
+  },
+  {
+    "revision": "3fde5592050adeb2a8956c2df809ccba",
+    "url": "/static/media/rain.3fde5592.svg"
+  }
+]);
